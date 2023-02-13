46bd1619f31cd3a5e7f067553481e92cda755b15
ian-iglesias <ianiglesias6798@gmail.com>
2023-02-13 16:41:50 -0300

    resulocao

---
+++ script.js
@@ -0,0 +1,52 @@
+// const booleano1 = false
+// const booleano2 = true
+
+// O "if" APENAS confirma se o que está entre parênteses é "true", caso seja, o que está entre chaves será executado.
+
+// if(booleano1){
+//     alert("booleano1 é true!")
+// }else{
+//     alert("booleano1 é false!")
+// };
+
+// O "else" é opcional. Ocorre caso o que está entre parêntese no "if" for "false"
+
+// if(booleano2){
+//     alert("booleano2 é true!")
+// }else{
+//     alert("booleano2 é false!")
+// };
+
+// const booleano3 = true
+
+// if(booleano1 === booleano2){
+//     alert("Os booleanos 1 e 2 são iguais!")
+// }else if(booleano2 === booleano3){
+//     alert("Os booleanos 2 e 3 são iguais!")
+// }else if(booleano1 === booleano3){
+//     alert("Os booleanos 1 e 3 são iguais!")
+// }else{
+//     alert("Todos os valores são diferentes!")
+// };
+
+const idadePessoa1 = 18
+// const idadePessoa2 = 18
+// const idadePessoa3 = 18
+// const idadePessoa4 = 18
+// const idadePessoa5 = 18
+
+if(idadePessoa1 >= 18){
+    alert("A pessoa 1 pode tirar a CNH!")
+}else if(idadePessoa1 < 18){
+    alert("A pessoa 1 não pode tirar a CNH!")
+}else{
+    alert("Idade da pessoa 1 não especificada!")
+};
+
+var media = 9
+
+if(media >= 5){
+    alert("Aprovado!")
+}else{
+    alert("Reprovado!")
+};
